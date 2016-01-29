248f0653985bff52e5a3d104a418c717cda0987e
nhs3108 <nhs3108@gmail.com>
2016-01-29 13:54:15 +0700

    First Interface Appearance

---
+++ app/src/main/java/com/example/nhs3108/framgiaalarm/activities/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.nhs3108.framgiaalarm.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.nhs3108.framgiaalarm.R;
+
+/**
+ * Created by nhs3108 on 1/29/16.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
